3d92d757161a407c669e684ac2aece0bade2f288
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2024-10-15 12:55:08 +0000

    start notes

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      int age = 15;
+      int year = 14;
+
+      //two == sign is checking for EQUALITY
+      boolean match = (age == year); 
+      System.out.println("Deos age match year?" + match);
+      
+      year = 15; // RE-ASSINGING
+      System.out.print(age == year);
+
+      //!= means "is NOT equal to"
+      System.out.println(age != year);
+
+      // REALTION OPERATORS 
+      // used in boolean expressions 
+      double pi = 3.14159;
+      System.out.println(pi > 0);
+      System.out.println(pi < 0);
+      System.out.println(pi >= 3.14);
+      System.out.println(pi <= 10);
+
+
+
+
+
    }
 }
